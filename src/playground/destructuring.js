593a28c46a53fe379aedef7daeca8a6c5c06d038
Peng Ye <yep0608@hotmail.com>
2019-08-29 16:51:54 +1000

    29/08

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -11,11 +11,24 @@ const person = {
 
 // const name = person.name;
 // const age = person.age;
-const { name, age } = person
+const { name: firstName = 'Anonymous', age } = person
 
-console.log(`${name} is ${age}.`);
+console.log(`${firstName} is ${age}.`);
 
 const { city, temp: temperature } = person.location;
 if (city && temperature) {
   console.log(`It's ${temperature} in ${city}.`);
 }
+
+
+const book = {
+  title: 'Ego is the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
+  }
+}
+
+const { name: publisherName = 'Self-Publisher' } = book.publisher
+
+console.log(publisherName);
