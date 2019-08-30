d8d6b9566d765f8f9596fdda017a483eebb66134
Peng Ye <yep0608@hotmail.com>
2019-08-30 22:45:47 +1000

    30/08

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -28,3 +28,26 @@ const store = createStore((state = { count: 0}, action) => {
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
+
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+store.dispatch({
+  type:'SET',
+  count: 101
+});
