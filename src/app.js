9ad609f72ab606e7ba747eef2735bad6dc579a01
Peng Ye <yep0608@hotmail.com>
2019-09-10 21:26:02 +1000

    10/09

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -14,9 +15,18 @@ const expenseOne = store.dispatch(addExpense({ description: 'Water bill', amount
 const expenseTwo = store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: -1000 }));
 const setTextFilterOne = store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000);
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+ReactDOM.render(jsx, document.getElementById('app'));
